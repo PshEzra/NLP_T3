30446c0c76752b4539a130f18ccfced752bd908f
ezrapasha <ezra.pasha@ui.ac.id>
2024-08-13 15:23:25 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+# Ezra Pasha Ramadhansyah - 2006597872
+
 from performance import Performance
 from trie_structure.levenshtein_trie import LevenshteinTrie
 from trie_structure.damerau_levenshtein_trie import DamerauLevenshteinTrie
@@ -15,7 +18,54 @@ dan atur parameter MAX_COST untuk setiap algoritma sebesar 2 ketika memanggil
 fungsi untuk pengambilan kandidat.
 """
 def main():
-    return None
+    j = open("saltik-revised.json", "r")
+    test = Performance(j.read())
+
+    lev_trie = LevenshteinTrie("./kbbi_dataset.txt")
+    lev_dict = LevenshteinDict("./kbbi_dataset.txt")
+    dalev_trie = DamerauLevenshteinTrie("./kbbi_dataset.txt")
+    dalev_dict = DamerauLevenshteinDict("./kbbi_dataset.txt")
+    
+    waktu_1 = time.time()
+    # lev_trie_best_accuracy,  = test.test_model_best_match(lev_trie)
+    candidate_accuracy, best_accuracy = test.test_model_candidate_match(lev_trie)
+    waktu_2 = time.time()
+    waktu = waktu_2 - waktu_1
+    print("[ Ezra Pasha Ramadhansyah - 2006597872 ]")
+    print("--------lev-trie--------")
+    print("Best Accuracy :", best_accuracy)
+    print("Candidate Accuracy :", candidate_accuracy)
+    print("Total Time :", waktu)
+
+    waktu_1 = time.time()
+    # lev_trie_best_accuracy,  = test.test_model_best_match(lev_trie)
+    candidate_accuracy, best_accuracy = test.test_model_candidate_match(lev_dict)
+    waktu_2 = time.time()
+    waktu = waktu_2 - waktu_1
+    print("--------lev-dict--------")
+    print("Best Accuracy :", best_accuracy)
+    print("Candidate Accuracy :", candidate_accuracy)
+    print("Total Time :", waktu)
+
+    waktu_1 = time.time()
+    # lev_trie_best_accuracy,  = test.test_model_best_match(lev_trie)
+    candidate_accuracy, best_accuracy = test.test_model_candidate_match(dalev_trie)
+    waktu_2 = time.time()
+    waktu = waktu_2 - waktu_1
+    print("--------damerau-lev-trie--------")
+    print("Best Accuracy :", best_accuracy)
+    print("Candidate Accuracy :", candidate_accuracy)
+    print("Total Time :", waktu)
+
+    waktu_1 = time.time()
+    # lev_trie_best_accuracy,  = test.test_model_best_match(lev_trie)
+    candidate_accuracy, best_accuracy = test.test_model_candidate_match(dalev_dict)
+    waktu_2 = time.time()
+    waktu = waktu_2 - waktu_1
+    print("--------damerau-lev-dict--------")
+    print("Best Accuracy :", best_accuracy)
+    print("Candidate Accuracy :", candidate_accuracy)
+    print("Total Time :", waktu)
 
 if __name__ == '__main__':
     main()
--- performance.py
+++ performance.py
@@ -1,7 +1,80 @@
 
+# Ezra Pasha Ramadhansyah - 2006597872
+
+import json
+
 """
 TODO: Gunakan file performance.py ini untuk membuat class Performance yang 
 nantinya akan digunakan untuk menghitung akurasi (Best Match Accuracy & Candidate Match) 
 dan run-time
 """
+
+class Performance:
     
+    def __init__(self, test_file):
+        self.test_dict = dict()
+        json_file = json.loads(test_file)
+        self.parse_json(json_file)
+
+    def parse_json(self, json_file):
+        amount = 800
+        num = 0
+
+        for i in json_file:
+            num +=1
+            self.test_dict[i] = list()
+            for j in json_file[i]:
+                self.test_dict[i].append(j['typo'])
+            
+            if num == amount: break
+
+    def test_model_candidate_match(self, model):
+        # print("Start Testing Best Candidate...")
+        total_non_word = 0
+        total_correct_candidate = 0
+        total_correct_first = 0
+
+        key_amount = 0
+        
+        for key in self.test_dict.keys():
+            for non_word in self.test_dict[key]:
+                total_non_word += 1
+                candidate_list = model.get_candidates(non_word, 2)
+
+                if key == candidate_list[0]:
+                    total_correct_first += 1
+
+                if key not in candidate_list:
+                    continue
+                pos = candidate_list.index(key)
+                if key != candidate_list[pos]:
+                    continue
+                total_correct_candidate += 1
+
+            key_amount += 1
+
+            # print("Finished for",key)
+            # print("Completeness", (key_amount/len(self.test_dict.keys())) * 100,"%")
+        
+        return (total_correct_candidate / total_non_word) * 100, (total_correct_first / total_non_word) * 100
+    
+    '''
+    def test_model_best_match(self, model):
+        print("Start Testing Best Match...")
+        total_non_word = 0
+        total_correct_candidate = 0
+
+        key_amount = 0
+        
+        for key in self.test_dict.keys():
+            for non_word in self.test_dict[key]:
+                total_non_word += 1
+                candidate_list = model.get_candidates(non_word, 5)
+                if key == candidate_list[0]:
+                    total_correct_candidate += 1
+            key_amount += 1
+            print("Finished for",key)
+            print("Completeness", (key_amount/len(self.test_dict.keys())) * 100,"%")
+        
+        return (total_correct_candidate / total_non_word) * 100
+    '''
